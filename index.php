e7e27ca0816e40e57b92b287316b18b49eaf1a95
VICTOR050913 <foovictor08@gmail.com>
2026-03-25 11:51:54 +0800

    siap

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Borang Permohonan</title>
+    <title>Permohonan Pinjaman Laptop</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -16,42 +16,58 @@
 
         <form action="process.php" method="POST" class="form-box">
 
-            <label class="label">Nama Penuh</label>
-            <input type="text" name="nama" class="input">
+            <div class="form-grid">
 
-            <label class="label">Alamat</label>
-            <textarea name="alamat" class="textarea"></textarea>
+                <div class="form-group">
+                    <label class="label">Nama Penuh</label>
+                    <input type="text" name="nama" class="input">
+                </div>
 
-            <label class="label">Nombor Telefon</label>
-            <input type="number" name="telefon" class="input">
+                <div class="form-group">
+                    <label class="label">Alamat</label>
+                    <textarea name="alamat" class="textarea"></textarea>
+                </div>
 
-            <label class="label">Tarikh</label>
-            <input type="date" name="tarikh" class="input">
+                <div class="form-group">
+                    <label class="label">Nombor Telefon</label>
+                    <input type="number" name="telefon" class="input">
+                </div>
 
-            <label class="label">Jenis Laptop</label>
-            <select name="jenis" class="input">
-                <option value="">-- Pilih --</option>
-                <option>Dell</option>
-                <option>HP</option>
-                <option>Lenovo</option>
-                <option>Asus</option>
-            </select>
+                <div class="form-group">
+                    <label class="label">Tarikh</label>
+                    <input type="date" name="tarikh" class="input">
+                </div>
+
+                <div class="form-group">
+                    <label class="label">Jenis Laptop</label>
+                    <select name="jenis" class="input">
+                        <option value="">-- Pilih --</option>
+                        <option>Dell</option>
+                        <option>HP</option>
+                        <option>Lenovo</option>
+                        <option>Asus</option>
+                    </select>
+                </div>
+
+            </div>
 
             <label class="label">Spesifikasi</label>
             <div class="group">
-                <label><input type="checkbox" name="spec[]" value="RAM 8GB" class="check"> RAM 8GB</label>
-                <label><input type="checkbox" name="spec[]" value="RAM 16GB" class="check"> RAM 16GB</label>
-                <label><input type="checkbox" name="spec[]" value="SSD 512GB" class="check"> SSD 512GB</label>
+                <label><input type="checkbox" name="spec[]" value="RAM 8GB"> RAM 8GB</label>
+                <label><input type="checkbox" name="spec[]" value="RAM 16GB"> RAM 16GB</label>
+                <label><input type="checkbox" name="spec[]" value="SSD 512GB"> SSD 512GB</label>
             </div>
 
-            <label class="label">Tempoh</label>
+            <label class="label">Tempoh Pinjaman</label>
             <div class="group">
-                <label><input type="radio" name="tempoh" value="1 Bulan" class="radio"> 1 Bulan</label>
-                <label><input type="radio" name="tempoh" value="3 Bulan" class="radio"> 3 Bulan</label>
+                <label><input type="radio" name="tempoh" value="1 Bulan"> 1 Bulan</label>
+                <label><input type="radio" name="tempoh" value="3 Bulan"> 3 Bulan</label>
             </div>
 
-            <label class="label">Alasan</label>
-            <textarea name="alasan" class="textarea"></textarea>
+            <div class="form-group full">
+                <label class="label">Alasan Permohonan</label>
+                <textarea name="alasan" class="textarea"></textarea>
+            </div>
 
             <div class="btn-group">
                 <button type="submit" class="btn-submit">Hantar</button>
